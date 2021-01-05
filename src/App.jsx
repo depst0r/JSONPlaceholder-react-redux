e3a767753b60759a5ca73d76972ab7f532cf0da9
depst0r <depstor90@gmail.com>
2021-01-05 13:43:52 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewsList from './Components/NewsList/NewsList'
+import './App.scss'
+
+export const App = () => {
+  return<>
+  <NewsList />
+    <h1>App</h1>
+  </>
+}
---
+++ src/Components/Header/NewsList/NewsList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewsList = () => {
+    return<>
+        <h1>NewsList</h1>
+    </>
+}
