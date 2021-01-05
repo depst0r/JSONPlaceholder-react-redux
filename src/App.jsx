81f388a9fb824c99507620ef4e503d20e690e9b5
depst0r <depstor90@gmail.com>
2021-01-05 16:47:14 +0300

    render'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchPosts, fetchUsers, fetchComments } from './Redux/Actions/action'
+import { useDispatch, useSelector } from 'react-redux'
 
 import './App.scss'
 import { NewsList } from './Components/NewsList/NewsList'
 
 export const App = () => {
+
+  const dispatch = useDispatch()
+  const selector = useSelector(state => state.rootReducer)
+
+  useEffect(() => {
+    dispatch(
+      fetchPosts()
+    )
+  }, [])
+
+  useEffect(() => {
+    dispatch(
+      fetchUsers()
+    )
+  }, [])
+
+  useEffect(() => {
+    dispatch(
+      fetchComments()
+    )
+  }, [])
+
   return<>
-  <NewsList  />
+    <NewsList  />
     <h1>App</h1>
   </>
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import reducers from './Redux/Reducers/reducers'
+
+const store = createStore(reducers, applyMiddleware(logger, thunk))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={ store }>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
