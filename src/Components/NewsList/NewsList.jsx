a1478ef31509c93948686a637609984fa90ba165
depst0r <depstor90@gmail.com>
2021-01-08 11:56:39 +0300

    new-test

--- src/Components/NewsList/NewsList.jsx
+++ src/Components/NewsList/NewsList.jsx
@@ -5,43 +5,54 @@ import { combimePost } from '../../Redux/Actions/action'
 
 export const NewsList = () => {
 
-    const dispatch = useDispatch()
-    const selector = useSelector(state => state.rootReducer)
+  const dispatch = useDispatch()
+  const selector = useSelector(state => state.rootReducer)
 
 
-    useEffect(() => {
-        dispatch(
-          fetchPosts()
-        )
-      }, [])
-    
-      useEffect(() => {
-        dispatch(
-          fetchUsers()
-        )
-      }, [])
-    
-      useEffect(() => {
-        dispatch(
-          fetchComments()
-        )
-      }, [])
+  useEffect(() => {
+    dispatch(
+      fetchPosts()
+    )
+  }, [])
+
+  useEffect(() => {
+    dispatch(
+      fetchUsers()
+    )
+  }, [])
+
+  useEffect(() => {
+    dispatch(
+      fetchComments()
+    )
+  }, [])
 
 
-useEffect(() => {
- const news =  selector.posts?.map(post => ({ ...post, user: selector.users?.find(user => post.userId === user.id) }))
- return dispatch(
+  useEffect(() => {
+    const combinePost = selector.posts?.map(post => ({ ...post, user: selector.users?.find(user => post.userId === user.id) }))
+    return dispatch(
       combimePost(
         {
-          post: news
+          post: combinePost,
         }
       )
-  )
+    )
+  }, [selector.posts, selector.users])
+// console.log(selector.post.post?.map(res => res.user.name))
+console.log('eddddddddddr', selector.post?.post)
 
-}, [selector.posts, selector.users])
+if (selector.post?.post === undefined) {
+  console.log('loading')
+} else {
+  console.log('hjrrr', selector.post?.post);
+}
 
-
-    return<>
-        <h1>NewsList</h1>
-    </>
+  return <>
+  {/* {selector.post.post?.map((res, i) => ( 
+  <p key={i}>{res.title}
+  </p>
+   )
+  )} */}
+    <h1>NewsList</h1>
+  </>
 }
